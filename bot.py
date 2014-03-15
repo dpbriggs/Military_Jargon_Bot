f0ed862e6fa358492122c2837755e2b8ecfd7798
iMultiPlay <imultiplay123@gmail.com>
2014-03-14 21:56:35 -0400

    Comment formatting most done

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import csv
 import praw
 import configparser
 from itertools import groupby
-
+from pprint import pprint
 class Military_Jargon(object):
     def __init__(self):
         #read config file
@@ -26,10 +26,12 @@ class Military_Jargon(object):
         
         #Get top threads from /r/military stories
         self.stories = self.get_threads()
-
+        global j
+        j = self.stories
         self.matched_jargon = self.jargon_finder()
         global k
         k = self.matched_jargon
+        pprint(k)
         
     def _read_wiki(self):
         #Convert string of numbers seperated by commas and spaces into a list of str-type numbers
@@ -48,12 +50,22 @@ class Military_Jargon(object):
         #We will now look for jargon
         for i in range(0, len(self.stories)):
             for k in self.stories[i][1]:
-                if k in self.jargon and k != '':
-                    #Append post_id, the jargon, and the explaination
+                if k in self.jargon and k != '' and not any(k in j for j in matches):
+                    #Append post, the jargon, and the explaination
                     matches.append((self.stories[i][0], k, self.jargon[k]))
+                                        
         matches = [list(v) for i, v in groupby(matches, key=lambda t: t[0])]
-        return matches
-    
+        comment_bucket = []
+        comment_start = '\n\n#This is an automated translation so there may be some errors. \n\n ***** \n\n'
+        comment_end = '\n\n ***** \n\n###Please reply or PM if I did something incorrect or missed some jargon \n\n`Bot by /u/Davess1`'
+        for threads in matches:
+            comment =  'Jargon | Translation \n :----|:----- \n '
+            for jargon in threads:
+                comment = comment + jargon[1] + ' |' + jargon[2] + ' \n '
+            comment_bucket.append((threads[0][0], comment_start+comment+comment_end))
+            
+        return comment_bucket
+        
     def get_threads(self):
         #Get top threads and break text up for iterating later
         hold = []
@@ -63,7 +75,7 @@ class Military_Jargon(object):
                 op_text = posts.selftext.upper() #all uppercase
                 op_text = ''.join([x for x in op_text if x.isalpha() or x.isspace()]) #Kill puncuation
                 op_text = op_text.split(' ')
-                hold.append((posts.id, op_text))
+                hold.append((posts, op_text))
         return hold
     
     def _read_config(self, file):
